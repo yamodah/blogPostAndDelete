4f21a336da5131aeb653619ebb194c6d94c2df06
yamodah <yassinedahlek@gmail.com>
2021-10-28 17:28:36 -0400

    delted extra comments

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -6,6 +6,7 @@ import React, { useState } from "react";
  * When the form is submitted, a new post is created and the form contents cleared.
  */
 function PostCreate({posts, postsSettingMethod}) {
+  
   const intialFormState ={
     type:"Text",
     content:""
@@ -14,25 +15,16 @@ function PostCreate({posts, postsSettingMethod}) {
 
   const handleSubmission =(event)=>{
     event.preventDefault()
-    //console.log(...posts)
     postsSettingMethod([...posts,{type:formData.type,content:formData.content}])
-    //console.log([...posts,formData])
     setFormData(intialFormState)
-   
   }
+
   const handleChange = ({ target }) => {
     setFormData({
       ...formData,
       [target.name]: target.value,
     });
   };
-  // TODO: When the form is submitted, a new post should be created, and the form contents cleared.
-
-  // For the tests to pass, the form below must have:
-  // - a `name="create"` attribute
-  // - one child `<button>` with a `type="submit"` attribute
-  // - one child `<select>` with a `name="type"` attribute
-  // - one child `<textarea>` or `<input>` (not both at the same time) with a `name="content"`
 
   return (
     <form name="create" onSubmit={handleSubmission}>
